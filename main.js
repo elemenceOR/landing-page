5bebfc255e4e4fe4df79ee488f7bd7d982e63e56
Sadeep Ariyarathna <52843991+elemenceOR@users.noreply.github.com>
2023-08-17 12:56:02 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
+const primaryHeader = document.querySelector('.primary-header');
+
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible')
+        ? primaryNav.setAttribute("aria-expanded", false)
+        : primaryNav.setAttribute("aria-expanded", true);
+    primaryNav.toggleAttribute("data-visible");
+    primaryHeader.toggleAttribute('data-overlay');
+})
+
